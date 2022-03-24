e10d5e3d845732b9df95b2ed8e335d6428a1f3a0
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2022-03-24 18:15:30 -0300

    animation sidebar

--- main.js
+++ main.js
@@ -24,3 +24,16 @@ new Chart(chart, {
     resposive: true
   }
 })
+
+// show or hide sidebar
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () => {
+  sidebar.style.display = 'block'
+})
+
+closeBtn.addEventListener('click', () => {
+  sidebar.style.display = 'none'
+})
